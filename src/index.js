825ad859a5df8e34f92908b7906b59c404bbf33b
中山遼太郎 <71915489+ryotaro-tenya0727@users.noreply.github.com>
2021-12-30 23:18:46 +0900

    【追加】タスクを未完了から完了に移す機能追加

--- src/index.js
+++ src/index.js
@@ -11,26 +11,52 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  //button(完了)生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    //押された完了ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncomplete(deleteButton.parentNode);
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    //div以下を初期化
+    addTarget.textContent = null;
+
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //divタグの子要素に書く要素設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
+  //button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncomplete(deleteButton.parentNode);
   });
 
   //divにli
   div.appendChild(li);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
-  console.log(div);
 
   document.getElementById("incomplete-list").appendChild(div);
+};
+
+const deleteFromIncomplete = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
