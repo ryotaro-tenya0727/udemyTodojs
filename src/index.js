1ecb59b012b61c3d50b14ae452164cd980424d23
中山遼太郎 <71915489+ryotaro-tenya0727@users.noreply.github.com>
2021-12-30 21:57:24 +0900

    タスクの追加機能

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div
+  const div = document.createElement("div");
+  div.classname = "list-row";
+  //li
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divにli
+  div.appendChild(li);
+
+  document.getElementById("incomplete-list").appendChild(div);
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
